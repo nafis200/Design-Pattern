a55f0084d3b081a22d3b9f4d3dbe97103ab37893
nafis ahamed <nafisahamed14@gmail.com>
2026-03-13 14:24:29 +0600

    hellow logger

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+public class Logger
+{
+
+    public Logger()
+    {
+        Console.WriteLine("Logger create ");
+    }
+    public void log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+      Logger logger = new Logger();
+
+      logger.log("heelo world");   
+    }
+}
--- obj/Debug/net8.0/desginpattern.AssemblyInfo.cs
+++ obj/Debug/net8.0/desginpattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("desginpattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe36afff1c5f309a49192c0c6b8d5ea02d025f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c87abc9a2bd752832c61d60efb99bbbc359c5e70")]
 [assembly: System.Reflection.AssemblyProductAttribute("desginpattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("desginpattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
